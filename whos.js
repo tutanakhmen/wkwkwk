cbb7493cb6b2af45efb8050bbd4696ecd0cbb159
Hasan Albana <hasanbena02@gmail.com>
2024-07-22 20:50:32 +0700

    task: done

--- whos.js
+++ whos.js
@@ -1,30 +1,24 @@
 function nyala() {
     let toggle1 = document.getElementById("toggle1")
-    let toggle2 = document.getElementById("toggle2")
-    let toggle3 = document.getElementById("toggle3")
 
     let lampu1 = document.getElementById("lampu1")
     let lampu2 = document.getElementById("lampu2")
     let lampu3 = document.getElementById("lampu3")
+    let lampu4 = document.getElementById("lampu4")
 
     if (toggle1.checked) {
         lampu1.src = "asset/images/on.gif"
+        lampu2.src = "asset/images/on.gif"
+        lampu3.src = "asset/images/on.gif"
+        lampu4.src = "asset/images/on.gif"
+
+
     } else {
         lampu1.src = "asset/images/off.gif"
-
-    }
-
-    if (toggle2.checked) {
-        lampu2.src = "asset/images/on.gif"
-    } else {
         lampu2.src = "asset/images/off.gif"
-
-    }
-
-    if (toggle3.checked) {
-        lampu3.src = "asset/images/on.gif"
-    } else {
         lampu3.src = "asset/images/off.gif"
+        lampu4.src = "asset/images/off.gif"
 
     }
+
 }
