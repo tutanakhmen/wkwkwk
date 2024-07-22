62e107b49e6e43e77743fcd97adc3db4f057d2c8
Hasan Albana <hasanbena02@gmail.com>
2024-07-22 19:14:29 +0700

    testing javascript

--- whos.js
+++ whos.js
@@ -1,14 +1,30 @@
-function saklar(params) {
-    let toggle = document.getElementById("toggle-default")
+function nyala() {
+    let toggle1 = document.getElementById("toggle1")
+    let toggle2 = document.getElementById("toggle2")
+    let toggle3 = document.getElementById("toggle3")
 
-    console.log("testing", toggle.checked)
     let lampu1 = document.getElementById("lampu1")
-    if(params == "on") {
+    let lampu2 = document.getElementById("lampu2")
+    let lampu3 = document.getElementById("lampu3")
+
+    if (toggle1.checked) {
         lampu1.src = "asset/images/on.gif"
-    }
-    if (params == "off") {
+    } else {
         lampu1.src = "asset/images/off.gif"
+
     }
-    return lampu1
-  }  
-  
+
+    if (toggle2.checked) {
+        lampu2.src = "asset/images/on.gif"
+    } else {
+        lampu2.src = "asset/images/off.gif"
+
+    }
+
+    if (toggle3.checked) {
+        lampu3.src = "asset/images/on.gif"
+    } else {
+        lampu3.src = "asset/images/off.gif"
+
+    }
+}
